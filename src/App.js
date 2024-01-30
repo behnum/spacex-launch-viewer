62708500b5b7b1170db57c3be22863332f74e31c
Behnam Baharmand <bbaharmand@gmail.com>
2024-01-30 08:00:23 +0100

    Init SpaceX launch data and button handlers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+import { useState } from 'react'
+import useSpaceXLaunches from '../useSpaceXLaunches'
+
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>SpaceX-Launch-Viewer</h1>
+  const { launches, loading } = useSpaceXLaunches()
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const handleNextButtonClick = () => {
+    // TODO: Implement this
+  }
+
+  const handleRandomButtonClick = () => {
+    // TODO: Implement this
+  }
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>SpaceX-Launch-Viewer</h1>
+    </>
+  )
 }
 export default App
